2a5dc06fabbf40baf60143f42ad4727e3f36a656
Adam Jones <domdomegg+git@gmail.com>
2022-05-15 17:19:32 +0100

    Improve CLI output message

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,25 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 
-import makeServer from '.';
+import server from '.';
 
 console.log('aws-ses-v2-local: starting server...');
-makeServer()
-  .then(() => {
-    console.log('aws-ses-v2-local: server up and running');
+server()
+  .then((s) => {
+    let address = s.address();
+    if (address && typeof address !== 'string') {
+      if (address.address === '127.0.0.1' || address.address === '::') {
+        address = `http://localhost:${address.port}`;
+      } else if (address.family === 'IPv4') {
+        address = `http://${address.address}:${address.port}`;
+      } else if (address.family === 'IPv6') {
+        address = `http://[${address.address}]:${address.port}`;
+      } else {
+        address = `${address.address}:${address.port}`;
+      }
+    }
+
+    console.log(`aws-ses-v2-local: server running${address ? ` at ${address}` : ''}`);
   })
   .catch((e) => {
     console.log('aws-ses-v2-local: failed to start server');
